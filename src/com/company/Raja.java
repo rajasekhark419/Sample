f78733f677f2403f1f9d9909d0ee7a97b3caf67a
user <rajasekhark419>
2020-02-29 12:29:50 +0530

    FirstCommit

---
+++ src/com/company/Raja.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public  class Raja{
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello");
+        System.out.println("Hiiii");
+
+
+    }
+}
